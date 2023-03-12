2be46a78ce2fb26c37419abd77800fadae39b405
Omar Osman <omar.osman@toptal.com>
2023-03-12 18:04:31 +0200

    Add service annotation and make gradlew executable

--- src/main/java/me/ommhoa/linkhandler/service/LanguageService.java
+++ src/main/java/me/ommhoa/linkhandler/service/LanguageService.java
@@ -6,9 +6,11 @@ import me.ommhoa.linkhandler.repository.LanguageRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class LanguageService {
     private static final Logger logger = LoggerFactory.getLogger(LanguageService.class);
 
